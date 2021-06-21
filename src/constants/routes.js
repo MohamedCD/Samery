9c4d555107fe052f89d2f41b82a29fd8264c0195
Mohamed Chahdi Dkikar <45308318+MohamedCD@users.noreply.github.com>
2021-06-21 03:48:05 +0200

    Add files via upload

---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
+export const WATCHED = '/watched';
+export const WATCH_LIST = '/watch-list';
+export const ADD = '/add';
